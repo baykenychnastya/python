b2f29b015ca80818578a8f37e2205210374816e4
Nastya14 <baykenych.nastya@gmail.com>
2021-09-22 22:02:20 +0300

    created a new function

--- programTask2.py
+++ programTask2.py
@@ -22,23 +22,23 @@ def printMatrix ( mat ):
           print ( "{:4d}".format(x), end = "" )
       print ()
 
+def generateMatrix(sizeMatrix):
+    mat = [[0] * sizeMatrix for i in range(sizeMatrix)]
+
+    for i in range(sizeMatrix):
+        mat[i][i] = (i + 1) * (i + 2)
+    return mat
+
 
 while True:
-    print('To exit the program, press 0')
+    print("To exit the program, press 0")
+    sizeMatrix = readPositivInteger('Enter the dimension of the matrix:','The dimension must be represented by a positive number!')
 
-    length = readPositivInteger('Enter the dimension of the matrix:', 'The dimension must be represented by a positive number!')
-
-    if length == -1:
+    if sizeMatrix == -1:
         continue
-    elif length == 0:
+    elif sizeMatrix == 0:
         break
-
-    mat = [[0]*length for i in range(length)]
-
-    for i in range(length):
-        mat[i][i]=(i+1)*(i+2)
-
     print('The matrix is formed: ')
-    printMatrix(mat)
+    printMatrix(generateMatrix(sizeMatrix))
 
 print('The program is finished...')
