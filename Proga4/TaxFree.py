03ec8d526c74f4023057ef9e66a3ceb24bbb6bec
Nastya14 <baykenych.nastya@gmail.com>
2021-10-27 09:13:52 +0300

    Programming Task4

---
+++ Proga4/TaxFree.py
@@ -0,0 +1,135 @@
+import enum
+from Inputs import *
+from Input import *
+from datetime import datetime
+
+
+class TAX_FREE(object):
+
+    def __init__(self, ID=None, Company=None, Country=None, vat_rate=None, date_of_purchase=None, vat_code=None,
+                 date_of_tax_free_registration=None):
+        self.ID = Input(ID, 'ID', [VALIDATION.validateInteger, VALIDATION.validatePositive])
+        self.Company = Input(Company, 'Company', [VALIDATION.validateRequiredStringWithTrim])
+        self.Country = Input(Country, 'Country (Italy or Germany or France)', [lambda value: VALIDATION.validateEnumValue(value, CountryEnum)])
+        self.vat_rate = InputInteger(vat_rate, "Vat rate", [lambda x: 0 < x < 41])
+        self.date_of_purchase = InputDate(date_of_purchase, "Date of purchase", [VALIDATION.validateDateString])
+        self.vat_code = Input(vat_code, "Vat code (in kind VA***_**_***)", [lambda x: VALIDATION.patternValidation(x, "^VA.{3,3}_.{2,2}_.{3,3}$")])
+        self.date_of_tax_free_registration = InputDate(date_of_tax_free_registration, "Date of tax free registration", [VALIDATION.validateDateString, lambda x: VALIDATION.validateLowerDateInputFile(self.date_of_purchase, x, "Date of tax free registration must be more date of purchase")])
+
+        self.inputValues = Inputs([
+            self.ID,
+            self.Company,
+            self.Country,
+            self.vat_rate,
+            self.date_of_purchase,
+            self.vat_code,
+            self.date_of_tax_free_registration])
+
+    def validate(self):
+        self.inputValues.validate()
+
+    def checkIsValid(self):
+        return self.inputValues.get_isValid()
+
+    def get_ID(self):
+        return self.ID.value
+
+    def get_Company(self):
+        return self.Company.value
+
+    def get_Country(self):
+        return self.Country.value
+
+    def get_vat_rate(self):
+        return self.vat_rate.value
+
+    def get_date_of_purchase(self):
+        return self.date_of_purchase.value
+
+    def get_vat_code(self):
+        return self.vat_code.value
+
+    def get_date_of_tax_free_registration(self):
+        return self.date_of_tax_free_registration.value
+
+    def enterCompany(self):
+        value = input(f'Enter Company: ')
+        if self.set_Company(value) is False:
+            self.enterCompany()
+
+    @VALIDATION.validation_decorator(VALIDATION.validateRequiredStringWithTrim)
+    def set_Company(self, value):
+        self.Company.value = value
+        return True
+
+    def enterCountry(self):
+        value = input(f'Enter Country (Italy or Germany or France): ')
+        if self.set_Country(value) is False:
+            self.enterCountry()
+
+    @VALIDATION.validation_decorator(lambda value: VALIDATION.validateEnumValue(value, CountryEnum))
+    def set_Country(self, value):
+        self.Country.value = value
+        return True
+
+    def enterVat_Rate(self):
+        value = input(f'Enter VAT Rate: ')
+        if self.set_vat_rate(value) is False:
+            self.enterVat_Rate()
+
+    @VALIDATION.validation_decorator(lambda x: VALIDATION.validateInteger(x) and (0 < int(x) < 41))
+    def set_vat_rate(self, value):
+        self.vat_rate.value = int(value)
+        return True
+
+    def enterDate_of_tax_free_registration(self):
+        value = input(f'Enter Date of tax free registration in format YYYY-MM-DD: ')
+        if self.set_Date_of_tax_free_registration(value, self.date_of_purchase.value) is False:
+            self.enterDate_of_tax_free_registration()
+
+    @VALIDATION.validation_decorator_with_two_arguments(
+        lambda x, date_of_purchase: VALIDATION.validateDateString(x) and
+        VALIDATION.validateLowerDate(datetime.strptime(date_of_purchase, '%Y-%m-%d'), datetime.strptime(x, '%Y-%m-%d'), "Date of tax free registration must be more date of purchase"))
+    def set_Date_of_tax_free_registration(self, value, date_of_purchase):
+        self.date_of_tax_free_registration.value = value
+        return True
+
+    def enterVat_code(self):
+        value = input(f'Enter VAT CODE: ')
+        if self.set_vatCode(value) is False:
+            self.enterVat_code()
+
+    @VALIDATION.validation_decorator(lambda x: VALIDATION.patternValidation(x, "^VA.{3,3}_.{2,2}_.{3,3}$"))
+    def set_vatCode(self, value):
+        self.vat_code.value = value
+        return True
+
+    def enterDate_of_purchase(self):
+        value = input(f'Enter Date of purchase in format YYYY-MM-DD: ')
+        if self.set_Date_of_purchase(value) is False:
+            self.enterDate_of_purchase()
+
+    @VALIDATION.validation_decorator(VALIDATION.validateDateString)
+    def set_Date_of_purchase(self, value):
+        self.date_of_purchase.value = value
+        return True
+
+    def toJson(self):
+        return {
+            "ID": self.ID.value,
+            "Company": self.Company.value,
+            "Country": self.Country.value,
+            "vat_rate": self.vat_rate.value,
+            "date_of_purchase": str(self.date_of_purchase.value),
+            "vat_code": self.vat_code.value,
+            "date_of_tax_free_registration": str(self.date_of_tax_free_registration.value)
+        }
+
+    def display(self):
+        print(self.toJson())
+
+
+class CountryEnum(str, enum.Enum):
+    Germany = "Germany"
+    Italy = "Italy"
+    France = "France"
---
+++ Proga4/validationForTaxFree.py
@@ -0,0 +1,125 @@
+import re
+import datetime
+from functools import wraps
+
+
+class VALIDATION:
+
+    @staticmethod
+    def validation_decorator(checkIsValid):
+        def decorator(fun):
+            def wrapper(*args, **kwargs):
+                isValid = checkIsValid(args[1])
+
+                if isValid is False:
+                    return False
+                fun(*args)
+            return wrapper
+        return decorator
+
+    @staticmethod
+    def validation_decorator_with_two_arguments(checkIsValid):
+        def decorator(fun):
+            def wrapper(*args, **kwargs):
+                isValid = checkIsValid(args[1], args[2])
+
+                if isValid is False:
+                    return False
+                fun(*args)
+            return wrapper
+        return decorator
+
+    @staticmethod
+    def patternValidation(value, regEx):
+        pattern = re.compile(regEx)
+        if pattern.match(value):
+            return True
+
+        print(f'Invalid value {value}: must match the regular expression {regEx}')
+        return False
+
+    @staticmethod
+    def validateDateString(value):
+        try:
+            datetime.datetime.strptime(value, '%Y-%m-%d')
+            return True
+        except ValueError:
+            print("Incorrect data format, must be YYYY-MM-DD")
+            return False
+
+    @staticmethod
+    def validateInteger(value):
+        try:
+            int(value)
+            return True
+        except ValueError:
+            print(f'Value {value} must be integer')
+            return False
+
+    @staticmethod
+    def validatePositive(value):
+        if value < 0:
+            print(f'Value {value} must be positive')
+            return False
+
+        return True
+
+    @staticmethod
+    def validateEnumValue(value, Enum):
+        validValues = tuple(item.value for item in Enum)
+
+        if value in validValues:
+            return True
+
+        print(f'Not valid value: {value}')
+        return False
+
+    @staticmethod
+    def validateRequiredStringWithTrim(value):
+        if len(value.strip()) > 0:
+            return True
+
+        print("Value is required")
+        return False
+
+    @staticmethod
+    def validateLowerDateInputFile(value1, value2, invalidDataMessages):
+        if value1.value <= value2:
+            return True
+        print(invalidDataMessages)
+        return False
+
+    @staticmethod
+    def validateLowerDate(value1, value2, invalidDataMessages):
+        if value1 <= value2:
+            return True
+        print(invalidDataMessages)
+        return False
+
+    @staticmethod
+    def readInteger(inputMessage, invalidDataMessages):
+        data = input(inputMessage)
+
+        try:
+            data = int(data)
+        except ValueError:
+            print(invalidDataMessages)
+            return VALIDATION.readInteger(inputMessage, invalidDataMessages)
+
+        return data
+
+    @staticmethod
+    def readIntegerWithCheck(inputMessage, isValid, invalidInputMessage=''):
+        data = input(inputMessage)
+
+        try:
+            data = int(data)
+        except ValueError:
+            print('Data must be represented by a positive number!')
+            return VALIDATION.readIntegerWithCheck(inputMessage, isValid, invalidInputMessage)
+
+        if not isValid(data):
+            print(invalidInputMessage)
+            return VALIDATION.readIntegerWithCheck(inputMessage, isValid, invalidInputMessage)
+
+        return data
