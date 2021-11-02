aae334117d4f754b901774e0348c35e9032879e6
Nastya14 <baykenych.nastya@gmail.com>
2021-11-02 23:12:09 +0200

    Programming Task5

---
+++ Proga5/Inputs.py
@@ -0,0 +1,12 @@
+
+class Inputs(object):
+
+    def __init__(self, inputValues):
+        self.inputValues = inputValues
+
+    def validate(self):
+        for inputValue in self.inputValues:
+            inputValue.get_isValid()
+
+    def get_isValid(self):
+        return all([inputValue.get_isValid() for inputValue in self.inputValues])
